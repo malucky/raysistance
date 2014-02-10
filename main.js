d5afe1a053b4fa63562fa129d1177ed3742e311e
Hack Reactor Students <github@hackreactor.com>
2014-02-10 12:57:21 -0800

    Added modal to prompt for user name. Storing value in global scope.

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 $(function(){
   window.App = {};
-  window.App.name = prompt("what's your name??");
+
+  /*prompt for name*/
+  $('form').submit(function(e){
+    e.preventDefault();
+    $('#myModal').modal('toggle');
+    window.userName = $('#nameInput').val();
+  });
+  $('#myModal').modal();
+  
+  // window.App.name = prompt("what's your name??");
 });
