02fe676a6d56035bcc829006ce4f593d3a19cc5b
Ray Ma <rhm262@gmail.com>
2014-02-11 14:02:39 -0800

    Set up very basic express server and converted parts of index.html to index.jade

--- gameLogic.js
+++
@@ -1,7 +0,0 @@
-var gameLogic = function(thisPlayer) {
-  var app = window.raysistanceApp;
-  if (window.playerName === thisPlayer.get('name')) {
-    alert('your are the leader!!');
-    that.distributeIdentities();
-  }
-};
